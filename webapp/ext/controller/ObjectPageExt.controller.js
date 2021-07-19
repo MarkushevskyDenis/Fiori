48c6dc172a9eea9d370b367fa6dfd2718b501b7a
Denis1234123 <denis.markushevskij@gmail.com>
2021-07-19 15:30:44 +0300

    step 12

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -1,7 +1,23 @@
+var id;
 sap.ui.controller("by.smartpro.fioritest.fiori.ext.controller.ObjectPageExt", {
-    onInit : function(oEvent) { 
-        this.extensionAPI.attachPageDataLoaded(function (oEvent) {
-			sap.m.MessageToast.show(oEvent.context.getProperty("CategoryID"));
+
+	onInit: function (oEvent) {
+		this.extensionAPI.attachPageDataLoaded(function (oEvent) {
+			id = oEvent.context.getProperty("CategoryID");
+			sap.m.MessageToast.show(id);
+
 		});
-    }
+	},
+	onHello: function (oEvent) {
+		sap.m.MessageToast.show("Hello world");
+	},
+	onActivate: function () {
+		if (this.getOwnerComponent().getModel().getProperty("/Categories(" + id + ")/StatusID") != 1) {
+			this.getOwnerComponent().getModel().setProperty("/Categories(" + id + ")/StatusID", 1);
+			this.getOwnerComponent().getModel().submitChanges();
+			this.getOwnerComponent().getModel().refresh();
+		} else {
+			sap.m.MessageToast.show("already is active");
+		}
+	}
 });
